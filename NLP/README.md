daea259c04cb89edb3758062817c97b10a153886
renjunxiang <renjunxiang1215@sina.com>
2018-01-19 16:56:07 +0800

    增加文本主题分类

--- NLP/README.md
+++ NLP/README.md
@@ -8,6 +8,10 @@ numpy=1.13.1<br>
 scikit-learn=0.19.1<br>
 jieba=0.39<br>
 ## 原理说明
+### 监督学习：supervised_classify.py
 将分词后的样本（英文自动按空格，中文分词后需按空格分割）通过sklearn.feature_extraction.text模块转为哈希格式减小存储开销，然后通过常用的机器学习分类模型如SVM和KNN进行学习和预测。本质为将文本转为稀疏矩阵作为训练集的数据，结合标签进行监督学习。<br>
 ![ex1](https://github.com/renjunxiang/machine-learning/blob/master/NLP/文本分类.png)
+### 非监督学习：LDA.py
+利用jieba分词，将分词后的样本队词频计数存为字典，再转为dataframe形式的稀疏矩阵。队稀疏矩阵进行ALS分解，转为文本-主题矩阵*主题-词语矩阵。<br>
+![ex1](https://github.com/renjunxiang/machine-learning/blob/master/NLP/文本主题分类.png)
 
---
+++ NLP/supervised_classify.py
@@ -0,0 +1,153 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, HashingVectorizer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.externals import joblib
+import jieba
+
+jieba.setLogLevel('WARN')
+
+class text_classify():
+    def __init__(self,
+                 language='English',
+                 model_exist=False,
+                 model_path=None,
+                 model_name='SVM',
+                 hashmodel='CountVectorizer',
+                 savemodel=False,
+                 train_dataset=None,
+                 test_data=None):
+        '''
+        :param language: 语种,中文将调jieba先分词
+        :param model_exist: 模型是否存在
+        :param model_path: 模型路径
+        :param model_name: 机器学习分类模型,SVM,KNN,Logistic
+        :param hashmodel: 哈希方式:CountVectorizer,TfidfTransformer,HashingVectorizer
+        :param savemodel: 保存模型
+        :param train_dataset: 训练集[[数据],[标签]]
+        :param test_data: 测试集[数据]
+        '''
+        self.language=language
+        self.model_exist = model_exist
+        self.model_path = model_path
+        self.model_name = model_name
+        self.hashmodel = hashmodel
+        self.savemodel = savemodel
+        self.train_dataset = train_dataset
+        self.test_data = test_data
+
+    def sentence_cut(self): #对中文进行分词处理
+        train_dataset = self.train_dataset
+        test_data = self.test_data
+        if self.language == 'Chinese':
+            train_dataset[0]=[' '.join(jieba.lcut(i)) for i in train_dataset[0]]
+            test_data=[' '.join(jieba.lcut(i)) for i in test_data]
+        return train_dataset,test_data
+
+    def data_transform(self):  # 全部数据转稀疏
+        train_dataset,test_data = self.sentence_cut()
+        train_data, train_label = train_dataset[0], train_dataset[1]  # 分离数据和标签
+        hashmodel = self.hashmodel
+        if hashmodel == 'CountVectorizer':  # 只计数
+            count_train = CountVectorizer()
+            train_data_hashcount = count_train.fit_transform(train_data)  # 训练数据转哈希计数
+            count_test = CountVectorizer(vocabulary=count_train.vocabulary_)  # 测试数据调用训练词库
+            test_data_hashcount = count_test.fit_transform(test_data)  # 测试数据转哈希计数
+        elif hashmodel == 'TfidfTransformer':  # 计数后计算tf-idf
+            count_train = CountVectorizer()
+            train_data_hashcount = count_train.fit_transform(train_data)  # 训练数据转哈希计数
+            count_test = CountVectorizer(vocabulary=count_train.vocabulary_)  # 测试数据调用训练词库
+            test_data_hashcount = count_test.fit_transform(test_data)  # 测试数据转哈希计数
+            tfidftransformer = TfidfTransformer()
+            train_data_hashcount = tfidftransformer.fit(train_data_hashcount).transform(train_data_hashcount)
+            test_data_hashcount = tfidftransformer.fit(test_data_hashcount).transform(test_data_hashcount)
+        elif hashmodel == 'HashingVectorizer':  # 哈希计算
+            vectorizer = HashingVectorizer(stop_words=None, n_features=10000)
+            train_data_hashcount = vectorizer.fit_transform(train_data)  # 训练数据转哈希后的特征,避免键值重叠导致过大有一个计算的
+            test_data_hashcount = vectorizer.fit_transform(test_data)  # 测试数据转哈希后的特征
+        return train_data_hashcount, train_label, test_data_hashcount
+
+    def model_train(self):
+        train_data_hashcount, train_label, test_data_hashcount = self.data_transform()
+        model_path = self.model_path
+        if self.model_exist == False:  # 如果不存在模型,调训练集训练
+            model_name = self.model_name
+            if model_name == 'KNN':
+                model = KNeighborsClassifier(n_neighbors=min(len(train_label), 5))  # 调用KNN,近邻=5
+                model.fit(train_data_hashcount, train_label)
+            elif model_name == 'SVM':
+                model = SVC(kernel='linear', C=1.0)  # 核函数为线性,惩罚系数为1
+                model.fit(train_data_hashcount, train_label)
+            elif model_name == 'Logistic':
+                model = LogisticRegression(solver='liblinear',C=1.0)  # 核函数为线性,惩罚系数为1
+                model.fit(train_data_hashcount, train_label)
+
+            if self.savemodel == True:
+                joblib.dump(model, model_path)  # 保存模型
+        else:  # 存在模型则直接调用
+            model = joblib.load(model_path)
+        return model
+
+    def model_predict(self):
+        model = self.model_train()
+        train_data_hashcount, train_label, test_data_hashcount = self.data_transform()
+        result = model.predict(test_data_hashcount)#对测试集进行预测
+        return result
+
+
+if __name__ == '__main__':
+    print('example:English')
+    train_dataset = [['he likes apple',
+                      'he really likes apple',
+                      'he hates apple',
+                      'he really hates apple'],
+                     ['possitive', 'possitive', 'negative', 'negative']]
+    print('train data\n',
+          pd.DataFrame({'data':train_dataset[0],
+                        'label':train_dataset[1]},
+                       columns=['data','label']))
+    test_data = ['she likes apple',
+                 'she really hates apple',
+                 'tom likes apple',
+                 'tom really hates apple'
+                 ]
+    test_label = ['possitive', 'negative', 'possitive', 'negative']
+    text_classify_try = text_classify(train_dataset=train_dataset,
+                                      test_data=test_data,
+                                      model_name='SVM')
+    result = text_classify_try.model_predict()
+    print('score:', np.sum(result == np.array(test_label)) / len(result))
+    result = pd.DataFrame({'data': test_data,
+                           'label': test_label,
+                           'predict': result},
+                          columns=['data', 'label', 'predict'])
+    print('test\n',result)
+
+    print('example:Chinese')
+    train_dataset = [['国王喜欢吃苹果',
+                      '国王非常喜欢吃苹果',
+                      '国王讨厌吃苹果',
+                      '国王非常讨厌吃苹果'],
+                     ['正面', '正面', '负面', '负面']]
+    print('train data\n',
+          pd.DataFrame({'data':train_dataset[0],
+                        'label':train_dataset[1]},
+                       columns=['data','label']))
+    test_data = ['涛哥喜欢吃苹果',
+                 '涛哥讨厌吃苹果',
+                 '涛哥非常喜欢吃苹果',
+                 '涛哥非常讨厌吃苹果']
+    test_label = ['正面', '负面', '正面', '负面']
+    text_classify_try = text_classify(train_dataset=train_dataset,
+                                      test_data=test_data,
+                                      model_name='SVM',
+                                      language='Chinese')
+    result = text_classify_try.model_predict()
+    print('score:', np.sum(result == np.array(test_label)) / len(result))
+    result = pd.DataFrame({'data': test_data,
+                           'label': test_label,
+                           'predict': result},
+                          columns=['data', 'label', 'predict'])
+    print('test\n',result)
