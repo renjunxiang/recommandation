7b3cfe4de9d4392a637547f76bc44a71006a1f2e
renjunxiang <renjunxiang1215@sina.com>
2018-01-21 18:08:48 +0800

    文本分类+评分

--- demo.py
+++ demo.py
@@ -1,13 +1,9 @@
-from NLP.sentence_transform import sentence_transform
+import json
+from NLP.supervised_classify import
 
-train_data = ['全面从严治党',
-              '国际公约和国际法',
-              '中国航天科技集团有限公司']
-test_data = ['全面从严测试']
-print('sentence_transform(train_data=train_data,hash=False)\n',
-      sentence_transform(train_data=train_data, hash=False))
-print('sentence_transform(train_data=train_data,hash=True)\n',
-      sentence_transform(train_data=train_data, hash=True))
-m, n = sentence_transform(train_data=train_data, test_data=test_data, hash=True)
-print('sentence_transform(train_data=train_data,test_data=test_data,hash=True)\n',
-      'train_data\n', m, '\ntest_data\n', n)
+with open('D:/github/machine-learning/NLP/data/keyword.json', encoding='utf-8') as f:
+    keyword = json.load(f)
+with open('D:/github/machine-learning/NLP/data/train_data.json', encoding='utf-8') as f:
+    train_data = json.load(f)
+
+print(keyword,train_data)
