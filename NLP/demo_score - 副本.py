462068b13d49c7cbe742a669aee9b2ca5ca1cf16
renjunxiang <renjunxiang1215@sina.com>
2018-01-23 18:50:12 +0800

    增加词向量处理

--- NLP/demo_score - 副本.py
+++ NLP/demo_score - 副本.py
@@ -17,32 +17,9 @@ result = supervised_classify(language='Chinese',
                              model_exist=False,
                              model_path='D:/github/machine-learning/NLP/data/demo_score/model.m',
                              model_name='SVM',
+                             vector=True,
                              hashmodel='CountVectorizer',
                              savemodel=True,
                              train_dataset=[list(X_train), list(y_train)],
                              test_data=list(X_test))
 print('score:', np.sum(result == np.array(y_test)) / len(result))
-
-predict_evaluate = pd.DataFrame({'document': X_test,
-                                 'label': y_test,
-                                 'predict': result},
-                                columns=['document', 'label', 'predict'])
-predict_evaluate=predict_evaluate.reset_index(drop=True)
-# predict_evaluate.to_excel('D:/github/machine-learning/NLP/data/demo_score/predict.xlsx')
-predict_evaluate_wrong=predict_evaluate.loc[predict_evaluate.loc[:,'label']!=
-                                            predict_evaluate.loc[:,'predict'],:]
-predict_evaluate_wrong.to_excel('D:/github/machine-learning/NLP/data/demo_score/predict.xlsx',
-                                index=False)#分类错误的数据保存下来
-
-
-# result = supervised_classify(language='Chinese',
-#                              model_exist=True,
-#                              model_path='D:/github/machine-learning/NLP/data/demo_score/model.m',
-#                              model_name='SVM',
-#                              hashmodel='CountVectorizer',
-#                              savemodel=True,
-#                              train_dataset=[list(X_train), list(y_train)],
-#                              test_data=list(['服务态度好差哦']))
-
-print(result)
-vector=True
--- NLP/supervised_classify.py
+++ NLP/supervised_classify.py
@@ -5,12 +5,14 @@ from sklearn.externals import joblib
 import pandas as pd
 import numpy as np
 from sentence_transform import sentence_transform
+from sentence_2_vec import sentence_2_vec
 
 
 def supervised_classify(language='English',
                         model_exist=False,
                         model_path=None,
                         model_name='SVM',
+                        vector=True,
                         hashmodel='CountVectorizer',
                         savemodel=False,
                         train_dataset=None,
@@ -25,30 +27,39 @@ def supervised_classify(language='English',
     :param train_dataset: 训练集[[数据],[标签]]
     :param test_data: 测试集[数据]
     '''
-    train_data_hashcount, test_data_hashcount = sentence_transform(train_data=train_dataset[0],
+    if vector == True:
+        train_data_transform, test_data_transform = sentence_2_vec(train_data=train_dataset[0],
                                                                    test_data=test_data,
-                                                                   language=language,
-                                                                   hash=True,
-                                                                   hashmodel=hashmodel)
+                                                                   size=50,
+                                                                   window=5,
+                                                                   min_count=1)
+        train_data_transform=[sum(i)/len(i) for i in train_data_transform]
+        test_data_transform = [sum(i)/len(i) for i in test_data_transform]
+    else:
+        train_data_transform, test_data_transform = sentence_transform(train_data=train_dataset[0],
+                                                                       test_data=test_data,
+                                                                       language=language,
+                                                                       hash=True,
+                                                                       hashmodel=hashmodel)
     train_label = train_dataset[1]
     model_path = model_path
     if model_exist == False:  # 如果不存在模型,调训练集训练
         model_name = model_name
         if model_name == 'KNN':
             model = KNeighborsClassifier(n_neighbors=min(len(train_label), 5))  # 调用KNN,近邻=5
-            model.fit(train_data_hashcount, train_label)
+            model.fit(train_data_transform, train_label)
         elif model_name == 'SVM':
             model = SVC(kernel='linear', C=1.0)  # 核函数为线性,惩罚系数为1
-            model.fit(train_data_hashcount, train_label)
+            model.fit(train_data_transform, train_label)
         elif model_name == 'Logistic':
             model = LogisticRegression(solver='liblinear', C=1.0)  # 核函数为线性,惩罚系数为1
-            model.fit(train_data_hashcount, train_label)
+            model.fit(train_data_transform, train_label)
 
         if savemodel == True:
             joblib.dump(model, model_path)  # 保存模型
     else:  # 存在模型则直接调用
         model = joblib.load(model_path)
-    result = model.predict(test_data_hashcount)  # 对测试集进行预测
+    result = model.predict(test_data_transform)  # 对测试集进行预测
     return result
 
 
